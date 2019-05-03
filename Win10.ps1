14553b485b7079c8d19c89fdd046eae59ac981b2
POPCORNrules <tony.arredondo5@gmail.com>
2019-05-03 15:07:10 +0000

    Upload files to ''

---
+++ Win10.ps1
@@ -0,0 +1,44 @@
+Write-Output "`n" # New Line for separation
+
+"{0, 10}: {1}" -f "Timestamp", (Get-Date -Format "yyyy-MM-dd HH:mm tt")
+"{0, 10}: {1}" -f "Computer", $env:COMPUTERNAME
+"{0, 10}: {1}" -f "User", "$env:USERNAME"
+
+$AppList = @(
+
+    "*Weather*",
+    "*zune*",
+    "*Solitaire*",
+    "*MicrosoftOfficeHub*",
+    "*Skype*",
+    "*Feedback*",
+    "*Onenote*",
+    "*Phone*",
+    "*Wallet*",
+    "*Getstarted*"
+
+)
+
+foreach ($App in $AppList) {
+
+    Get-AppxPackage -Name $App | ForEach-Object {"Removing: $($_.PackageFullName)"}
+    Get-AppxPackage -Name $App | Remove-AppxPackage
+	Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -like $App} | Remove-AppxProvisionedPackage -Online
+
+}
+
+#Win10 - Disable Advertising
+
+New-item -Path HKLM:\Software\Policies\Microsoft\Windows -Name CloudContent -ErrorAction SilentlyContinue
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" -Name DisableSoftLanding -Value 1 -Type DWord -Force                # Don't show Windows Tips
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" -Name DisableWindowsConsumerFeatures -Value 1 -Type DWord -Force    # Disable Windows Consumer Features
+
+#Remove Suggested Apps
+reg load HKU\Default_User C:\Users\Default\NTUSER.DAT
+Set-ItemProperty -Path Registry::HKU\Default_User\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name SystemPaneSuggestionsEnabled -Value 0
+Set-ItemProperty -Path Registry::HKU\Default_User\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name PreInstalledAppsEnabled -Value 0
+Set-ItemProperty -Path Registry::HKU\Default_User\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name OemPreInstalledAppsEnabled -Value 0
+reg unload HKU\Default_User
+
+#Remove Pinned Suggested Apps
+Import-StartLayout -LayoutPath "DefaultLayout.xml" -MountPath "C:\"
